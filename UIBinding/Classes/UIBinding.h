907048c2280a783fa3802df121c6f809126f5e00
Peter Davidson <PeterDavidson@Boiseinc.com>
2013-12-31 10:26:27 -0700

    Cleared out old stuff

--- UIBinding/Classes/UIBinding.h
+++
@@ -1,9 +0,0 @@
-//
-//  UIBinding.h
-//  BoisePaperMobile
-//
-//  Created by Peter Davidson on 12/27/13.
-//  Copyright (c) 2013 Boise Paper Holdings. All rights reserved.
-//
-
-#import "UIBindingManager.h"
