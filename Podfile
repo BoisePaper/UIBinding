4cf005e5cabb3e69d7e9291fdc51545a03a54db5
Joel Sonoda <JoelSonoda@Boiseinc.com>
2014-01-07 16:24:56 -0700

    Add test for binding manager using OCMockito

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform  :ios, '6.0'
+
+target 'UIBindingTests', :exclusive => true do
+  pod 'OCMockito', '~> 1.0'
+end
+
--- UIBindingTests/UIBindingManagerTests.m
+++ UIBindingTests/UIBindingManagerTests.m
@@ -6,22 +6,17 @@
 //  Copyright (c) 2014 Peter Davidson. All rights reserved.
 //
 
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+#define MOCKITO_SHORTHAND
+#import <OCMockito/OCMockito.h>
+
 #import <XCTest/XCTest.h>
 #import "UIBoundView.h"
 #import "UIBindingManager.h"
 
-@interface UIBindingManagerTestsMockView : UIView<UIBoundView>
-@property NSObject* model;
-@end
 
-@implementation UIBindingManagerTestsMockView
-@synthesize fieldName;
--(void) bindToModel:(NSObject *) model
-{
-	self.model = model;
-}
-
-@end
 
 @interface UIBindingManagerTests : XCTestCase
 
@@ -31,13 +26,32 @@
 
 - (void)testSimpleBinding
 {
-    UIBindingManagerTestsMockView* view = [[UIBindingManagerTestsMockView alloc] init];
-	NSObject* model = [[NSObject alloc] init];
+    UIView<UIBoundView>* view = mockObjectAndProtocol([UIView class], @protocol(UIBoundView));
+	NSObject* model = mock([NSObject class]);
 	
 	UIBindingManager* instance = [[UIBindingManager alloc] init];
 	[instance bindModel:model toView:view];
 	
-	XCTAssertEqual(model, view.model, @"Model should be bound to view.");
+	[verify(view) bindToModel:model];
+}
+
+- (void)testTableViewBinding
+{
+	UIView<UIBoundView>* view = mockObjectAndProtocol([UIView class], @protocol(UIBoundView));
+	NSObject* model = mock([NSObject class]);
+	UITableViewCell* cell = mock([UITableViewCell class]);
+	[given([cell subviews]) willReturn:@[view]];
+	
+	UITableView* tableView = mock([UITableView class]);
+	NSIndexPath* path = [NSIndexPath indexPathForRow:0 inSection:0];
+	[given([tableView numberOfSections]) willReturnUnsignedInteger:1];
+	[given([tableView numberOfRowsInSection:0]) willReturnUnsignedInteger:1];
+	[given([tableView cellForRowAtIndexPath:equalTo(path)]) willReturn:cell];
+	
+	UIBindingManager* instance = [[UIBindingManager alloc] init];
+	[instance bindModel:model toView:tableView];
+	
+	[verify(view) bindToModel:model];
 }
 
 @end
